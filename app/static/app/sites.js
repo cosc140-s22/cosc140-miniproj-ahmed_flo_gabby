04765de0b9cf7071311aea31c4c6c202cb8a3afe
Ahmed Kamran <akamran@colgate.edu>
2022-05-15 17:25:03 +0100

    index page form w/o js

--- app/static/app/sites.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("search").addEventListener("click", (e) => {
-  const form = document.querySelector("form");
-  const search = form.querySelector("input").value;
-  if (search !== "") {
-    form.submit();
-  }
-});

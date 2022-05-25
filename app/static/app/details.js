55ef3d592138394f6801258d3cf470a6ca7e5209
Ahmed Kamran <akamran@colgate.edu>
2022-05-25 15:37:16 +0100

    implemented geocoding locations in python

--- app/static/app/details.js
+++ app/static/app/details.js
@@ -5,50 +5,15 @@ const show_caption = (id) => {
   document.getElementById(id).style.opacity = 1;
 };
 
-const is_valid_postal_code = (postal_code) => {
-  const postal_code_regex = /^[a-z]{1,2}\d[a-z\d]?\s*\d[a-z]{2}$/i;
-  return postal_code.match(postal_code_regex) !== null;
-};
-
-const api_req = async (search) => {
-  let result = localStorage.getItem(search);
-  if (result == null) {
-    const res = await fetch(
-      `https://nominatim.openstreetmap.org/search?q=${search}&format=json`
-    );
-    const data = await res.json();
-    // Store the result of the API request locally to not have to repeat it when not needed
-    localStorage.setItem(search, JSON.stringify(data));
-    console.log("From API request: ");
-    return data;
-  } else {
-    console.log("From local storage: ");
-    return JSON.parse(result);
-  }
-};
-
-const get_map_config = async () => {
-  const address = document.getElementById("site-location").text;
-  const postal_code = address.split(" ").slice(-2).join(" ");
-  const search = is_valid_postal_code(postal_code) ? postal_code : address;
-  const data = await api_req(search);
-
-  console.table({
-    searched: search,
-    got: data,
-  });
-
+const get_map_config = () => {
+  const params = new URLSearchParams(location.search);
   const config = {
-    lat: 0,
-    lon: 0,
-    zoom: 0,
+    lat: params.get("lat"),
+    lon: params.get("lon"),
+    zoom: 16,
   };
 
-  try {
-    config.lat = parseFloat(data[0]["lat"]);
-    config.lon = parseFloat(data[0]["lon"]);
-    config.zoom = 16;
-  } catch (e) {
+  if (config.lat === null || config.lon == null) {
     //Default to Lat and Lon of Wales if error in data
     config.lat = 52.2928116;
     config.lon = -3.73893;
@@ -58,26 +23,22 @@ const get_map_config = async () => {
   return config;
 };
 
-const display_map = async () => {
-  try {
-    map_config = await get_map_config();
-    document.getElementById("map").style.height = "400px";
-    const map = new ol.Map({
-      target: "map",
-      layers: [
-        new ol.layer.Tile({
-          source: new ol.source.OSM(),
-        }),
-      ],
-      view: new ol.View({
-        center: ol.proj.fromLonLat([map_config.lon, map_config.lat]),
-        zoom: map_config.zoom,
+const display_map = () => {
+  map_config = get_map_config();
+  document.getElementById("map").style.height = "400px";
+  const map = new ol.Map({
+    target: "map",
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM(),
       }),
-    });
-    document.getElementById("loader").style.display = "none";
-  } catch (error) {
-    document.getElementById("map-error-msg").style.display = "block";
-  }
+    ],
+    view: new ol.View({
+      center: ol.proj.fromLonLat([map_config.lon, map_config.lat]),
+      zoom: map_config.zoom,
+    }),
+  });
+  document.getElementById("loader").style.display = "none";
 };
 
 window.addEventListener("load", display_map);
--- app/views.py
+++ app/views.py
@@ -4,10 +4,27 @@ from django.urls import reverse
 from .models import Site, SiteImage
 from django.contrib.auth.decorators import login_required
 from .forms import ReviewForm
+import re
+import requests
 
+def get_post_code(address):
+    post_code_regex = "[A-Z]{1,2}[0-9][A-Z0-9]? ?[0-9][A-Z]{2}"
+    return re.findall(post_code_regex,address)
 
-# Create your views here.
-def index(request: HttpRequest):
+def get_lat_lon(address, session):
+    post_code = get_post_code(address)
+    search =  address if len(post_code)==0 else post_code[0]
+    if(session.get(F'{search}-lat')==None or session.get(F'{search}-lon')==None):
+        data = requests.get(F"https://nominatim.openstreetmap.org/search?q={search}&format=json").json()
+        if(len(data) > 0):
+            '''
+                Store API requests in session to avoid searching places already found
+            '''
+            session[F'{search}-lat'] = data[0]["lat"]
+            session[F'{search}-lon'] = data[0]["lon"]
+    return (session.get(F'{search}-lat'), session.get(F'{search}-lon'))
+
+def index(request:HttpRequest):
     sites = Site.objects.all().order_by('title')
 
     search_req = request.GET.get('search')
@@ -37,7 +54,6 @@ def index(request: HttpRequest):
 
     return render(request, 'app/index.html', context)
 
-
 def random_image(request):
     img:SiteImage = SiteImage.objects.order_by("?").first()
     return redirect(img.image.url)
@@ -51,7 +67,16 @@ def details(request, site_id):
         'images': images if images.exists() else None,
         'reviews': reviews if reviews.exists() else None
     }
-    return render(request, 'app/show.html', context)
+
+    lat,lon = get_lat_lon(site.location, request.session)
+    if(lat and lon):
+        if(request.GET.get('lat')==lat and request.GET.get('lon')==lon):
+            return render(request, 'app/show.html', context)
+        else:
+            url = F"/sites/{site_id}?lat={lat}&lon={lon}"
+            return redirect(url)
+    else:
+        return render(request, 'app/show.html', context)
 
 @login_required
 def create_review(request, site_id):
